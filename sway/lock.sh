85d062641a97dd00f1058b787d29441d3b30128c
Matthew Lyon <matthew@matthewlyon.net>
2022-01-22 03:38:04 -0500

    mutes sound, then unmutes instead of play pause

--- sway/lock.sh
+++ sway/lock.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-playerctl pause
+pamixer -m
 swaylock --screenshots --clock --indicator-idle-visible \
         --indicator-radius 100 \
         --indicator-thickness 7 \
@@ -12,3 +12,4 @@ swaylock --screenshots --clock --indicator-idle-visible \
         --fade-in 0.1 \
         --effect-scale 0.5 --effect-blur 7x3 --effect-scale 2 \
         --effect-vignette 0.5:0.5
+pamixer -u
